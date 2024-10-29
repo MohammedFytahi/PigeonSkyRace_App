b3e6d57ad7a8ef7e3eaf7d5623927b4bb6672978
abderrahmane-03 <abdrahmanhafidi03@gmail.com>
2024-10-29 11:19:43 +0100

    project update

---
+++ src/test/java/pegion/pegionRacer/PegionRacerApplicationTests.java
@@ -0,0 +1,13 @@
+package pegion.pegionRacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegionRacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
