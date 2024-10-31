097656ac586f295422f55847bf03ba09bf03b325
MohammedFytahi <mohammedfytahi567@gmail.com>
2024-10-31 15:29:41 +0100

    PIG-10 gestion des pigeons

--- src/main/java/net/yc/race/track/service/PigeonService.java
+++ src/main/java/net/yc/race/track/service/PigeonService.java
@@ -3,7 +3,11 @@ package net.yc.race.track.service;
 import net.yc.race.track.model.Pigeon;
 import net.yc.race.track.repository.PigeonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Service
 public class PigeonService {
@@ -13,5 +17,18 @@ public class PigeonService {
 
     public Pigeon savePigeon(Pigeon pigeon) {
         return pigeonRepository.save(pigeon);
+    }
+
+    public List<Pigeon> findPigeons(){
+        return pigeonRepository.findAll();
+    }
+
+    public String deletePigeonById(Integer id) {
+        if (pigeonRepository.existsById(id)) {
+            pigeonRepository.deleteById(id);
+            return "Pigeon supprimé avec succès.";
+        } else {
+            return "Pigeon non trouvé.";
+        }
     }
 }
