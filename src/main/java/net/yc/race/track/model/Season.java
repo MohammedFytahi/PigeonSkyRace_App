3418f141600effaaf8bdf3fad8a8a1512250a760
MohammedFytahi <mohammedfytahi567@gmail.com>
2024-11-12 11:23:21 +0100

    PIG-19 SeasonService testing

--- src/main/java/net/yc/race/track/model/Season.java
+++ src/main/java/net/yc/race/track/model/Season.java
@@ -1,7 +1,9 @@
 package net.yc.race.track.model;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import net.yc.race.track.Enum.Status;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +11,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "season")
 @Data
 @NoArgsConstructor
+@Getter
+@Setter
 public class Season {
     @Id
     private String seasonId;
