1d426af4aace17a929e666eb907422a853e6e48a
abderrahmane-03 <abdrahmanhafidi03@gmail.com>
2024-11-15 17:23:43 +0100

    PIG-14 breeder classment

--- src/main/java/net/yc/race/track/repository/CompetitionRepository.java
+++ src/main/java/net/yc/race/track/repository/CompetitionRepository.java
@@ -6,6 +6,10 @@ import net.yc.race.track.model.Season;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CompetitionRepository extends MongoRepository<Competition, String> {
+    List<Competition> findBySeasonId(String seasonId);
+
 }
